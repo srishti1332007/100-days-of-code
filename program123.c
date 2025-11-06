c5fd57dc577f87f69534d0b53ad76841129dae7c
srishti1332007 <srishti1332007@gmail.com>
2025-11-06 10:00:00 +0530

    code

---
+++ program123.c
@@ -0,0 +1,39 @@
+//Q123: Read a text file and count the total number of characters, words, and lines. A word is defined as a sequence of non-space characters separated by spaces or newlines.//
+
+/*
+Sample Test Cases:
+Input 1:
+File: sample.txt (Content: Hello world\nThis is C programming)
+Output 1:
+Characters: 31
+Words: 5
+Lines: 2
+
+*/
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char ch, prev = ' ';
+    int chars = 0, words = 0, lines = 0;
+
+    fp = fopen("sample.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file.");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        chars++;
+        if (ch == '\n')
+            lines++;
+        if (!isspace(ch) && isspace(prev))
+            words++;
+        prev = ch;
+    }
+
+    fclose(fp);
+    printf("Characters: %d\nWords: %d\nLines: %d\n", chars, words, lines + 1);
+    return 0;
+}
